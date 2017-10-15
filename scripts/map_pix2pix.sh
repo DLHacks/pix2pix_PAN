117d06a396fa0244e3c2b18f4ca7929ebd09b280
OtsuboAtsushi <assanuts128@gmail.com>
2017-10-16 00:00:03 +0900

    fix typo

--- scripts/map_pix2pix.sh
+++ scripts/map_pix2pix.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=$1 python mytrain.py --dataroot ./datasets/maps --name pix2pix_map_10_14 --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --norm batch --pool_size 0 --niter 200 --niter_decay 200 --loadSize 256 --batchSize 4
+CUDA_VISIBLE_DEVICES=$1 python train.py --dataroot ./datasets/maps --name pix2pix_map_10_14 --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --norm batch --pool_size 0 --niter 200 --niter_decay 200 --loadSize 256 --batchSize 4
 
-CUDA_VISIBLE_DEVICES=$1 python mytest.py --dataroot ./datasets/maps --name pix2pix_map_10_14 --model pix2pix --which_model_netG unet_256 --which_direction AtoB --dataset_mode aligned --norm batch --loadSize 256
+CUDA_VISIBLE_DEVICES=$1 python test.py --dataroot ./datasets/maps --name pix2pix_map_10_14 --model pix2pix --which_model_netG unet_256 --which_direction AtoB --dataset_mode aligned --norm batch --loadSize 256
